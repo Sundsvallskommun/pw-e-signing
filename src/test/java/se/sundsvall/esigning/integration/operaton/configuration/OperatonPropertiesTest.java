54d22b8997c096c28f794eb387606d90e75b9bc6
AndreSoderlund <andre.soderlund@b3.se>
2026-06-18 15:19:22 +0200

    HYDRAN-2521: Add Operaton Feign client with OAuth2 registration and generated DTOs

---
+++ src/test/java/se/sundsvall/esigning/integration/operaton/configuration/OperatonPropertiesTest.java
@@ -0,0 +1,23 @@
+package se.sundsvall.esigning.integration.operaton.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import se.sundsvall.esigning.Application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = Application.class)
+@ActiveProfiles("junit")
+class OperatonPropertiesTest {
+
+	@Autowired
+	private OperatonProperties properties;
+
+	@Test
+	void testProperties() {
+		assertThat(properties.connectTimeout()).isEqualTo(5);
+		assertThat(properties.readTimeout()).isEqualTo(20);
+	}
+}
